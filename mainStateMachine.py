816b96275989a7d6694194b466aa7a30479afbc4
Gabriel Bertocco <gabrielcb958@gmail.com>
2019-10-09 10:56:30 -0300

    main file added

---
+++ mainStateMachine.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.insert(0, 'src')
+sys.path.insert(0, 'lib')
+from robot import Robot
+import numpy as np
+from numpy.linalg import norm
+import time
+
+from avoidObstacleFuzzy import avoidObstacleFuzzyController
+from gtgFuzzy import gtgFuzzyController
+
+def main():
+
+	print("Let's start it!")
+
+if __name__ == '__main__':
+	main()
