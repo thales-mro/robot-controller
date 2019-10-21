855e6f4911faf6e29fc3620bbd92b67affb06423
Thales Mateus Rodrigues Oliveira <ra148051@students.ic.unicamp.br>
2019-10-21 09:09:23 -0300

    Added PD and PI controllers for wall follow behavior

--- wallFollow.py
+++ wallFollow.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 class WallFollowController():
     """
     It implements methods for wall-follow-type controller using PID logic.
@@ -9,12 +7,17 @@ class WallFollowController():
     def __init__(self):
         self.left_wheel_speed = 1.5 # medium base speed for wheels
         self.right_wheel_speed = 1.5
-        self.k_p = 2 # empirical value
-        self.reference = 0.25 # in centimeters
+        self.k_p = 2.0 # empirical value
+        self.k_i = 0.1 # empirical value
+        self.k_d = 0.5 #empirical value
+        self.reference = 0.30 # in centimeters
+        self.pi_counter = 0
+        self.integral_error = 0.0
+        self.last_step_error = 0.0
 
-    def control(self, sensor_distance, direction='right'):
+    def p_control(self, sensor_distance, direction='right'):
         """
-        It performs proportional wall following control
+        It performs proportional control for wall following behavior
 
         Keyword arguments:
         sensor_distance -- the measured distance from sensor of interest
@@ -35,6 +38,67 @@ class WallFollowController():
             right_vel = self.right_wheel_speed
             print("Speed", left_vel)
 
+
+        return (left_vel, right_vel)
+
+    def pi_control(self, sensor_distance, direction='right'):
+        """
+        It performs proportional-integrative control for wall following behavior
+
+        Keyword arguments:
+        sensor_distance -- the measured distance from sensor of interest
+        direction -- direction related to which US sensor is analyzed
+            right (default): US sensor positioned at robot's right side
+            left: US sensor positioned at robot's left side
+        """
+        if sensor_distance >= 5.0:
+            return (0, 0)
+
+        if self.pi_counter % 10 == 0:
+            self.integral_error = 0
+
+        error = self.reference - sensor_distance
+        self.integral_error += error
+
+        if direction == 'right':
+            right_vel = (error * self.k_p) + (
+                self.integral_error * self.k_i) + self.right_wheel_speed
+            left_vel = self.left_wheel_speed
+            print("Speed", right_vel)
+        else:
+            left_vel = (error * self.k_p) + (self.integral_error * self.k_i) + self.left_wheel_speed
+            right_vel = self.right_wheel_speed
+            print("Speed", left_vel)
+
+        self.pi_counter += 1
+
+        return (left_vel, right_vel)
+
+    def pd_control(self, sensor_distance, direction='right'):
+        """
+        It performs proportional-derivative control for wall following behavior
+
+        Keyword arguments:
+        sensor_distance -- the measured distance from sensor of interest
+        direction -- direction related to which US sensor is analyzed
+            right (default): US sensor positioned at robot's right side
+            left: US sensor positioned at robot's left side
+        """
+        if sensor_distance >= 5.0:
+            return (0, 0)
+
+        error = self.reference - sensor_distance
+        diff_error = error - self.last_step_error
+        self.last_step_error = error
+
+        if direction == 'right':
+            right_vel = (error * self.k_p) + (diff_error * self.k_d) + self.right_wheel_speed
+            left_vel = self.left_wheel_speed
+            print("Speed", right_vel)
+        else:
+            left_vel = (error * self.k_p) + self.left_wheel_speed
+            right_vel = self.right_wheel_speed
+            print("Speed", left_vel)
 
         return (left_vel, right_vel)
         
